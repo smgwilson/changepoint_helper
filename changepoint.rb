c93d3731c76149e72f1d1444a3a58138024f81a6
Sheri Wilson <smgwilson37@gmail.com>
2015-02-17 20:41:18 -0800

    adds methods and refactors
    1. Added arrays to sum total hours and total percentages
    2. Added method “run_program” to control program flow

--- changepoint.rb
+++ changepoint.rb
@@ -2,8 +2,22 @@ class ChangepointHelper
 
   def initialize(hours = 40)
     @hours = hours
+    @all_percentages = Array.new
+    @all_hours = Array.new
   end
 
+  def run_program(do_next)
+    case do_next
+      when "start"
+        start_program
+      when "continue"
+        keep_going
+      when "end"
+        end_program
+    end
+  end
+
+  # get user selection on calculation to perform
   def start_program
     puts "Welcome to Changepoint Helper!"
     puts "Changepoint sucks, but this program will help it suck a little less :-D"
@@ -17,6 +31,20 @@ class ChangepointHelper
     print "> "
 
     @choice = $stdin.gets.chomp
+    select_calculation(@choice)
+
+  end
+
+  def select_calculation(option)
+    if option == "1"
+      calc_percentage (@hours)
+    elsif option == "2"
+      calc_project_hours (@hours)
+    else
+      puts "You didn't enter a valid selection."
+    end
+
+    keep_going
 
   end
 
@@ -34,6 +62,15 @@ class ChangepointHelper
 		# puts "The total hours worked are #{total_hours}"
 		percentage = (project_hours * 100) / total_hours
 		puts "Your percentage of time on this project was #{percentage}%"
+    @all_percentages.push (percentage)
+    sum = @all_percentages.inject{|sum,x| sum + x }
+    if sum == 100
+      puts "You're at 100%!"
+    elsif sum > 100
+      puts "You've exceeded 100%"
+    else
+      puts "You're up to #{sum}%"
+    end
 	end
 
 	def calc_project_hours (total_hours)
@@ -42,6 +79,15 @@ class ChangepointHelper
 		percentage = $stdin.gets.chomp.to_f
 		project_hours = (percentage * total_hours) / 100
 		puts "Hours spent on this project were #{project_hours}"
+    @all_hours << project_hours
+    sum = @all_hours.inject{|sum,x| sum + x}
+    if sum == @hours
+      puts "You're at #{hours} hours."
+    elsif sum > @hours
+      puts "You've exceeded your reported hours."
+    else
+      puts "You're up to #{sum}%"
+    end
 	end
 
 	def keep_going
@@ -49,30 +95,17 @@ class ChangepointHelper
 		print "> "
 		do_another = $stdin.gets.chomp.downcase
 		if do_another == "y"
-			run_program
+			select_calculation(@choice)
 		elsif do_another == "n"
 			exit(0)
 		else
 			"You didn't enter a valid selection."
 			keep_going
 		end
-	end
+  end
 
-	def run_program
-
-		if @choice == "1"
-			calc_percentage (@hours)
-		elsif @choice == "2"
-			calc_project_hours (@hours)
-		else
-			puts "You didn't enter a valid selection."
-		end
-
-		keep_going
-
-	end
 end
 
 cp = ChangepointHelper.new
-cp.start_program
-cp.run_program
+cp.run_program("start")
+
